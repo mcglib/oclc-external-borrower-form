4c95d0eff961c372e0f4139a3575762e6a19041f
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2019-03-15 12:56:17 -0400

    Udpayted

--- app/Http/Requests/Borrower.php
+++ app/Http/Requests/Borrower.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Yaml;
 
 class Borrower extends FormRequest
 {
@@ -29,10 +30,11 @@ class Borrower extends FormRequest
 	    'email' => 'required|email',
 	    'borrower_cat' => 'required',
 	    'telephone_no' => 'required|numeric',
-	    'spouse_name' => 'required_if:borrower_cat,value5',
+	    'spouse_name' => 'required_if:borrower_cat,value4',
 	    'address1' => 'required_if:borrower_cat,value7',
 	    'postal_code' => 'required_if:borrower_cat,value7',
 	    'city' => 'required_if:borrower_cat,value7',
+	    'home_institution' => 'required_if:borrower_cat,value9, value1',
         ];
     }
     /**
@@ -42,12 +44,16 @@ class Borrower extends FormRequest
 	    *     */
     public function messages()
     {
+	// Load the variables
+	$yamlContents = Yaml::parse(file_get_contents(base_path().'/borrowing_categories.yml'));
+	dd($yamlContents);
         return [
 	        'fname.required' => 'Your first name  is required',
 	        'lname.required' => 'Your last name  is required',
 	        'email.required' => 'Your email  is required',
 	        'borrower_cat.required' => 'Please select  a borrowing category',
 	        'telephone_no.required' => 'Please enter a phone number we can reach you at',
+	    	'spouse_name.required_if' => 'Please enter the name of your spouse',
         ];
     }
 }
