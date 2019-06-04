ab8a099487d3f603a4f2b2c533274df6f5001779
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2019-06-04 12:08:00 -0400

    Updated the code to prevent double clicks

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,26 @@
+ // jQuery plugin to prevent double submission of forms
+ $.fn.preventDoubleSubmission = function () {
+            $(this).on('submit', function (e) {
+                var $form = $(this);
+
+                if ($form.data('submitted') === true) {
+                    // Previously submitted - don't submit again
+                    alert('The form has already been submitted. Please wait.');
+                    e.preventDefault();
+                } else {
+                    // Mark it so that the next submit can be ignored
+                    // ADDED requirement that form be valid
+                    if($form.valid()) {
+                        $form.data('submitted', true);
+                    }
+                }
+            });
+
+            // Keep chainability
+            return this;
+ };
+
+
 $.fn.clear_address_values = function () {
 	 $("#address1").val("");
 	 $("#address2").val("");
@@ -7,12 +30,10 @@ $.fn.clear_address_values = function () {
 
 };
 $.fn.clear_home_inst_values = function () {
-	console.log("clear select opts");
 	 $("#home_institution").val("");
 
 };
 $.fn.clear_spouse_name_values = function () {
-	console.log("clear select opts");
 	 $("#spouse_name").val("");
 
 };
@@ -102,6 +123,8 @@ $(document).ready(function () {
     // Get the curr val
     $curr_val = $('select[name="borrower_cat"]').val();
     $.fn.update_fields_info($curr_val);
+
+    $('#store-form').preventDoubleSubmission();
 
     $('select[name="borrower_cat"]').change(function () {
 	$selected_val = $(this).val();
