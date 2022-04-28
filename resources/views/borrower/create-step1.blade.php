31a9c811c0050f7e65ceb70dfe7d6121f8e847f0
Deepak Chauhan <deepak.chauhan@mcgill.ca>
2022-04-28 15:47:37 -0400

    added changes to homeonlydivcheck

--- resources/views/borrower/create-step1.blade.php
+++ resources/views/borrower/create-step1.blade.php
@@ -53,14 +53,11 @@
 				    <label for="home_institution" class="control-label">Please enter the name of your home institution<span class="required">*</span></label>
 				    {!! Form::select('home_institution', array_merge(['' => 'Please select an institution'], $home_institutions), $borrower->home_institution ?? null, ['class' => 'form-control']) !!}
         		        </div>	
-				<div class="form-group">
-				    <label for="only_institution" class="control-label">Please enter the name of your institution<span class="required">*</span></label>
-				    {!! Form::select('only_institution', array_merge(['' => 'Please select an institution'], $only_institutions), $borrower->only_institution ?? null, ['class' => 'form-control']) !!}
-        		        </div>			
+						
 			    </div>
 				<div id="homeOnlyDivCheck" class="no-display">
 				<div class="form-group">
-				    <label for="only_institution" class="control-label">Please enter the name of your home institution<span class="required">*</span></label>
+				    <label for="only_institution" class="control-label">Please enter the name of home institution<span class="required">*</span></label>
 				    {!! Form::select('only_institution', array_merge(['' => 'Please select an institution'], $only_institutions), $borrower->only_institution ?? null, ['class' => 'form-control']) !!}
         		        </div>	
 			    </div>
